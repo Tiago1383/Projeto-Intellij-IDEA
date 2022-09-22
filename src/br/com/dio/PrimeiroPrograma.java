55959482fe2c52c31df9187fc90da02c4cea4062
Teigo1383 <teiweb58@gmail.com>
2022-09-22 20:57:22 -0300

    First Commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+import br.com.dio.Model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato(" Mimi", " Preto", 11);
+
+        System.out.println(gato);
+
+       /* int a = 5;
+        int b = 5;
+        System.out.println("Hello World! " + (a + b));*/
+    }
+}
